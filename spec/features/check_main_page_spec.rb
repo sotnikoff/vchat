101703131803157edfc62c550e2f34e6c4ccca03
Unknown <thehofch@gmail.com>
2018-05-20 01:55:26 +0300

    fix spec

--- spec/features/check_main_page_spec.rb
+++ spec/features/check_main_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'Main page' do
   it 'opens main page' do
     visit '/'
-    expect(page).to have_content 'Create chat'
+    expect(page).to have_content I18n.t('chats.index.create_chat')
   end
 end

d70d8ea27fec42cbf512767f03abdbffab25b332
Mikhail Sotnikov <sotnikoff@users.noreply.github.com>
2018-05-20 01:56:06 +0300

    Views (#4)
    * remove captcha, add some I18n
    
    * fix locals
    
    * fix spec

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
-    if verify_recaptcha(model: @message) && @message.save
+    if @message.save
       redirect_to chat_path(@message.chat_id)
     else
       render 'errors/common', status: :bad_request
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,5 @@
 class Chat < ApplicationRecord
   validates :title, length: { in: 3..50 }
   has_many :messages
+  scope :last_fifty, -> { order('created_at DESC').limit(50).all }
 end

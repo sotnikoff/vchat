eb284a25ec8eab055aa1eaef4ffdb532d8d8c138
Unknown <thehofch@gmail.com>
2018-05-19 12:15:31 +0300

    add operation + test

---
+++ spec/operations/convert_text_to_speech_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ConvertTextToSpeech do
+  describe 'Testing of return audio stream' do
+    it 'returns AWS class' do
+      output = ConvertTextToSpeech.call('hello!', 'Maxim', 'mp3')
+      expect(output.audio_stream.size).to be_a_kind_of(Integer)
+      expect(output.audio_stream.size).to be > 1
+    end
+  end
+end
